bb261806040e68e05a8a0816f4e44445facd96f5
pushkala-m <pushkala.m@lxisoft.com>
2020-03-02 12:16:48 +0530

    jira.bytatech.com:8080/browse/IN-6902

--- src/main/java/com/lxisoft/MockTest/model/UserRegistrationModel.java
+++ src/main/java/com/lxisoft/MockTest/model/UserRegistrationModel.java
@@ -1,21 +1,24 @@
 package com.lxisoft.MockTest.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user_registration")
 public class UserRegistrationModel 
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
+	
 	@Column(name="username")
 	private String name; 
 
 	@Column(name="email")
-	
 	private String email;  
 
 	@Column(name="password")
