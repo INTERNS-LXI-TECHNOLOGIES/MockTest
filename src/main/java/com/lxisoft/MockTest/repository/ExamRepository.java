a7fb86ad78834e691e84c32b8144408db0d2a7b4
pushkala-m <pushkala.m@lxisoft.com>
2020-03-03 10:48:53 +0530

    http://jira.bytatech.com:8080/browse/IN-6910, http://jira.bytatech.com:8080/browse/IN-6985

---
+++ src/main/java/com/lxisoft/MockTest/repository/ExamRepository.java
@@ -0,0 +1,12 @@
+package com.lxisoft.MockTest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lxisoft.MockTest.model.UserRegistrationModel;
+
+
+public interface ExamRepository extends JpaRepository<UserRegistrationModel, Long> 
+{
+
+
+}
