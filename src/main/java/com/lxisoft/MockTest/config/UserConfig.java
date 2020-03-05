57b9b4a002ca21e9a334fbc6f883e4c95c424b85
muhammedashiq <muhammed.ashiq.k.p@lxisoft.com>
2020-03-05 10:30:03 +0530

    http://jira.bytatech.com:8080/browse/IN-7104

--- src/main/java/com/lxisoft/MockTest/config/UserConfig.java
+++ src/main/java/com/lxisoft/MockTest/config/UserConfig.java
@@ -46,7 +46,7 @@ public class UserConfig extends WebSecurityConfigurerAdapter {
          http
              .authorizeRequests()
                  .antMatchers("/index").permitAll()
-                 .antMatchers("/userpage").hasRole("USER")
+                 .antMatchers("/userpage").access("hasRole('USER') or hasRole('ADMIN')")
                  .antMatchers("/adminpage").hasRole("ADMIN")
                  .and().formLogin().and()
                  .rememberMe();
