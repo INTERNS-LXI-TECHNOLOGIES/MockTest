17d6c0ff4bdbb2f0ba3f52da16ac1925c05d25de
pushkala-m <pushkala.m@lxisoft.com>
2020-03-06 14:55:32 +0530

    http://jira.bytatech.com:8080/browse/IN-7201

--- src/main/java/com/lxisoft/MockTest/controller/ExamController.java
+++ src/main/java/com/lxisoft/MockTest/controller/ExamController.java
@@ -8,12 +8,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
 
 
 import com.lxisoft.MockTest.model.SetTimerModel;
@@ -105,6 +103,12 @@ public class ExamController
 	public String logout()
 	{
 		return "logout";
+	}
+	@RequestMapping("/deletequestion")
+	public String Deletequestion()
+	{
+	 
+		return "viewall_qstn";
 	}
 }
 
--- src/main/java/com/lxisoft/MockTest/repository/UserRepository.java
+++ src/main/java/com/lxisoft/MockTest/repository/UserRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 package com.lxisoft.MockTest.repository;
 
@@ -12,18 +12,4 @@ public interface UserRepository extends JpaRepository<UserRegistration, Long>
 	UserRegistration findByUsername(String username);
 	
 }
-=======
 
-package com.lxisoft.MockTest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lxisoft.MockTest.model.UserRegistration;
-
-
-public interface UserRepository extends JpaRepository<UserRegistration, Long> 
-{
-	UserRegistration findByUsername(String username);
-	
-}
->>>>>>> abd88d318ee598928509cee3c2864eabfc31ed48
