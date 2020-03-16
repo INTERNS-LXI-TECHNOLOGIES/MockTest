8d6d2790cf971d570cd8bb55002d43022a157564
muhammedashiq <muhammed.ashiq.k.p@lxisoft.com>
2020-03-16 16:15:56 +0530

    http://jira.bytatech.com:8080/browse/IN-7549

--- src/main/java/com/lxisoft/MockTest/model/Question.java
+++ src/main/java/com/lxisoft/MockTest/model/Question.java
@@ -28,11 +28,20 @@ public class Question {
 	@Column(name = "level")
 	private String level;
 		 
-	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private List<Option> options;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "option_id", referencedColumnName = "id")
+	private Collection<QstnOption> options;
 	
 	public long getId() {
 		return id;
+	}
+
+	public Collection<QstnOption> getOptions() {
+		return options;
+	}
+
+	public void setOptions(Collection<QstnOption> options) {
+		this.options = options;
 	}
 
 	public void setId(long id) {
