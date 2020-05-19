11e0b7b51f1b2f4d8a8555a39ceeffcd09a0c4f1
pushkala-m <pushkala.m@lxisoft.com>
2020-05-19 19:50:12 +0530

    json data getting

--- MockTest_Springboot_Api/src/main/java/com/lxisoft/web/rest/MocktestControllerResource.java
+++ MockTest_Springboot_Api/src/main/java/com/lxisoft/web/rest/MocktestControllerResource.java
@@ -1,10 +1,24 @@
 package com.lxisoft.web.rest;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.lxisoft.service.UserService;
+import com.lxisoft.service.dto.UserDTO;
+
+import io.github.jhipster.web.util.PaginationUtil;
 
 /**
  * MocktestControllerResource controller
@@ -14,13 +28,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class MocktestControllerResource {
 
     private final Logger log = LoggerFactory.getLogger(MocktestControllerResource.class);
-
+//    private UserService userService;
+    @Autowired 
+    UserResource userRes;
+    
     /**
     * GET getRegistrationDetails
     */
     @GetMapping("/get-registration-details")
     public String getRegistrationDetails() {
         return "getRegistrationDetails";
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDTO>> getAll() {
+    	Pageable pageable=null;
+    	return userRes.getAllUsers(pageable);
+        
     }
     
 
