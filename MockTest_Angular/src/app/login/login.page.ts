4b357d82c573cc52736a97284aee693587a2c445
pushkala-m <pushkala.m@lxisoft.com>
2020-05-19 17:19:36 +0530

    routing test with pages

--- MockTest_Angular/src/app/login/login.page.ts
+++ MockTest_Angular/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { FolderPage } from '../folder/folder.page';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/folder/Home',
+      icon: 'mail'
+    },
+    {
+      title: 'Outbox',
+      url: '/folder/Outbox',
+      icon: 'paper-plane'
+    },
+    {
+      title: 'Favorites',
+      url: '/folder/Favorites',
+      icon: 'heart'
+    },
+    {
+      title: 'Archived',
+      url: '/folder/Archived',
+      icon: 'archive'
+    },
+    {
+      title: 'Trash',
+      url: '/folder/Trash',
+      icon: 'trash'
+    },
+    {
+      title: 'Spam',
+      url: '/folder/Spam',
+      icon: 'warning'
+    }
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
   }
-
+  async signIn(){
+    const modal = await this.modalCtrl.create({
+      component: FolderPage,
+      swipeToClose: true,
+      presentingElement: await this.modalCtrl.getTop() // Get the top-most ion-modal
+    });
+    return await modal.present();
+  }
 }
--- MockTest_Springboot_Api/src/main/java/com/lxisoft/web/rest/MocktestControllerResource.java
+++ MockTest_Springboot_Api/src/main/java/com/lxisoft/web/rest/MocktestControllerResource.java
@@ -22,5 +22,6 @@ public class MocktestControllerResource {
     public String getRegistrationDetails() {
         return "getRegistrationDetails";
     }
+    
 
 }
