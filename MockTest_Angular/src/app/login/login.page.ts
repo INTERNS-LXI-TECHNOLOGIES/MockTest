0063ed91979f6690fc9156c01e495935d399a903
pushkala-m <pushkala.m@lxisoft.com>
2020-05-25 16:38:23 +0530

    login page

--- MockTest_Angular/src/app/login/login.page.ts
+++ MockTest_Angular/src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { FolderPage } from '../folder/folder.page';
+import { ModalController,NavController } from '@ionic/angular';
+import{AdminpagePage} from '../adminpage/adminpage.page';
 import { UsersService } from '../users.service';
 import { Router } from '@angular/router';
 import { MenuController} from '@ionic/angular';
@@ -14,19 +14,21 @@ export class LoginPage implements OnInit {
 
   data;
   username;
+ 
   
   constructor(private modalCtrl: ModalController,
-    private userServ:UsersService,public menu: MenuController,
-    public router: Router) {}
+    private userServ:UsersService,public menu: MenuController,private nav:NavController,
+    private router: Router) {}
 
   ngOnInit() {
    
      this.users();
   }
-  // signIn(){
-  //   this.router
-  //     .navigateByUrl('../adminpage/adminpage.module', { replaceUrl: true })
-  // }
+  
+  signIn(){
+    // this.nav.navigateRoot(AdminpagePage);
+    // this.router.navigate=(['/adminpage']);
+    }
   users()
   {
     this.userServ.getData().subscribe(data => {
