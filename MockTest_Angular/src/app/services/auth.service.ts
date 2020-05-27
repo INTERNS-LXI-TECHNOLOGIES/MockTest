8078f3c503e377c6d791b2e442b7077a1ed18cb9
pushkala-m <pushkala.m@lxisoft.com>
2020-05-27 17:24:11 +0530

    auth service

--- MockTest_Angular/src/app/services/auth.service.ts
+++ MockTest_Angular/src/app/services/auth.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+  public isAuthenticated(): boolean {
+    // Return true or false
+    return true;
+    }
+    
+    public login(postData) {
+    // login actions
+    }
+    
+    
+    public signup(postData) {
+    // login actions
+    
+    }
+    
+    public logout() {
+    // logout actions
+    }
+    }
 }
