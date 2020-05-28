63ccc3a3f337a379ffc6fb001d7887d4c45ba617
muhammedashiq <muhammed.ashiq.k.p@lxisoft.com>
2020-05-28 18:59:07 +0530

    allQuestions

--- MockTest_Angular/src/app/pages/login/login-routing.module.ts
+++ MockTest_Angular/src/app/pages/login/login-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: LoginPage
     
-  },
-  {
-    path: 'adminpage',
-    loadChildren: () => import('../adminpage/adminpage.module').then( m => m.AdminpagePageModule)
   }
 ];
 
