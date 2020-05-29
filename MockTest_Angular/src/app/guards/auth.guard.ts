e01bbe0c5240de8a240b501e9b00d29c47326f33
pushkala-m <pushkala.m@lxisoft.com>
2020-05-29 19:40:56 +0530

    authentication router issues

--- MockTest_Angular/src/app/guards/auth.guard.ts
+++ MockTest_Angular/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree ,Router} from '@angular/router';
+import { CanActivate,Router} from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
@@ -8,11 +8,7 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  // canActivate(
-  //   next: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  
+
   constructor(public authService:AuthService,private router:Router){}
   canActivate() : boolean{
     console.log(this.authService.isAuthenticated());
