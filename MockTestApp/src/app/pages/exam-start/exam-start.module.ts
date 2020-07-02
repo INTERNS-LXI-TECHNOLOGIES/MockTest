e71cd6178ff459ee3dfdc0488a60668d8c3dc464
pushkala-m <pushkala.m@lxisoft.com>
2020-07-02 14:13:33 +0530

    theme set

--- MockTestApp/src/app/pages/exam-start/exam-start.module.ts
+++ MockTestApp/src/app/pages/exam-start/exam-start.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ExamStartPageRoutingModule } from './exam-start-routing.module';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { ExamStartPage } from './exam-start.page';
 
 @NgModule({
@@ -13,7 +13,8 @@ import { ExamStartPage } from './exam-start.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ExamStartPageRoutingModule
+    ExamStartPageRoutingModule, 
+    TranslateModule,
   ],
   declarations: [ExamStartPage]
 })
